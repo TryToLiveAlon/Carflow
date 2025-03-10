6f80e2d92ce0c48138b56510c3294ff6010d99f9
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-03-10 21:02:08 +0530

    Update mail.js

--- api/mail.js
+++ api/mail.js
@@ -5,7 +5,9 @@ const mailHandler = async (req, res) => {
         const { gmail, password, to, subject, content } = req.query;
 
         if (!gmail || !password || !to || !subject || !content) {
-            return res.status(400).json({ error: "Missing required parameters" });
+            return res.status(400).json({ error: "Missing required parameters",
+                                        provider: "https://t.me/TryToLiveAlon",
+            documentation: "https://death-docs.vercel.app/API/Quick%20Start"});
         }
 
         // ✅ Create Nodemailer Transporter
@@ -17,10 +19,14 @@ const mailHandler = async (req, res) => {
         // ✅ Send Email
         await transporter.sendMail({ from: gmail, to, subject, text: content });
 
-        res.json({ success: true, message: "Email sent successfully!" });
+        res.json({ success: true, message: "Email sent successfully!",
+                 provider: "https://t.me/TryToLiveAlon",
+            documentation: "https://death-docs.vercel.app/API/Quick%20Start"});
     } catch (error) {
         console.error("Email sending error:", error);
-        res.status(500).json({ error: "Failed to send email" });
+        res.status(500).json({ error: "Failed to send email",
+                             provider: "https://t.me/TryToLiveAlon",
+            documentation: "https://death-docs.vercel.app/API/Quick%20Start"});
     }
 };
 
