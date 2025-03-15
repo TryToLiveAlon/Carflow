74f38364e07948dbaae837ee0d92dafd16257415
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-03-15 20:12:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import movieHandler from "./api/movie.js";  
 import vehicleHandler from "./api/vehicle.js";  
 import gptHandler from "./api/gpt.js";
-import router from "./mail.js";
+import mailHandler from "./api/mail.js";
 
 const app = express();
 app.use(express.json());
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use("/api/movie", movieHandler);
 app.use("/api/vehicle", vehicleHandler);
 app.use("/api/gpt", gptHandler);
-app.use("/api/mail", router);
+app.use("/api/mail", mailHandler);
 
 app.use((req, res) => {
     res.status(404).json({ error: "404: NOT FOUND" });
