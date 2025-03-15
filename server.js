9c42d6a9f1216e114c2f230b088a1cbfbd2aa82c
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-03-15 20:55:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import mailHandler from "./api/mail.js";
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // ✅ API Routes
 app.use("/api/movie", movieHandler);
