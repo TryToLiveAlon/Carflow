d47b04d6791d49f7066ee44039b1ec1bc6f7a831
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-05-13 11:13:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import sendEmail from "./api/mail.js";
 import lyricsHandler from "./api/lyrics.js";
 import deepSeek from "./api/deepseek.js";
 import mistral from "./api/mistral.js";
+import mailHandler from "./api/anonymous-mail.js";
 
 
 const app = express();
@@ -18,6 +19,7 @@ app.use("/api/vehicle", vehicleHandler);
 app.use("/api/brain", brainHandler);
 app.use("/api/deepseek", deepSeek);
 app.use("/api/mistral", mistral)
+app.use("/api/mail-id", mailHandler);
 app.post("/api/mail", sendEmail); // Attach the mail handlera
 
 // 404 Route Handling
